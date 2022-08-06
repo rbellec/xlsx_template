c9ecd2b2887a097f56b80c7cd67d2c25123d9773
Raphael Bellec <raphael.bellec@gmail.com>
2022-08-06 17:12:03 +0200

    Create gem with bundle

---
+++ lib/xlsx_template.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "xlsx_template/version"
+
+module XlsxTemplate
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/xlsx_template/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module XlsxTemplate
+  VERSION = "0.1.0"
+end
